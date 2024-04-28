fb32647a2d474bf35b1e5136adb705fb95a8725c
Achira Dias <80286616+achiraDias@users.noreply.github.com>
2024-04-28 21:06:05 +0530

    Add white overlay

--- app/src/main/java/com/example/securesharev1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/securesharev1/ui/home/HomeFragment.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.securesharev1.R;
 import com.example.securesharev1.databinding.FragmentHomeBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeFragment extends Fragment {
 
+    private EditText sendInput;
+    private TextView textView;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -24,8 +29,20 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+        textView = binding.textHome;
+        sendInput = binding.editTextTextMultiLine;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Sending...", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.text_home)
+                        .setAction("Action", null).show();
+                sendData();
+            }
+        });
+
         return root;
     }
 
@@ -33,5 +50,9 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void sendData() {
+        textView.setText(sendInput.getText());
     }
 }
